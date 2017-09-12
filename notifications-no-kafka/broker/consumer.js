f69221d46fb0c6dee21eaaec4833366e76cc871d
zVictor <victor0110@msn.com>
2017-09-12 15:40:56 +0200

    add topic creation to kafka

--- notifications-no-kafka/broker/consumer.js
+++ notifications-no-kafka/broker/consumer.js
@@ -7,7 +7,13 @@ for (const key of Object.keys(supplies)) {
   consumer.init().then(() => {
     consumer.subscribe(key, 0, { time: LATEST_OFFSET }, (messageSet, topic, partition) => {
       messageSet.forEach((m) => {
-        console.log(topic, partition, m.offset, m.message.key, m.message.value.toString('utf8'));
+        console.log(
+          topic,
+          partition,
+          m.offset,
+          m.message.key && m.message.key.toString('utf8'),
+          m.message.value && m.message.value.toString('utf8'),
+        );
         supplies[key](
           m.message.value.toString('utf8'),
           m.message.key && m.message.key.toString('utf8'),
