941de30a0886510d94e1ba12842e1968a746da37
zVictor <victor0110@msn.com>
2018-03-20 18:14:44 +0100

    remove forgotten radiaction example

--- notifications-waiter/broker/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  DELIMITER: '__--',
-  RESULT_SUFFIX: '.output',
-  WAITER_TIMEOUT: 5000,
-};
--- notifications-waiter/definitions.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  MODULE_KEY: 'services/notifications-waiter',
-};
