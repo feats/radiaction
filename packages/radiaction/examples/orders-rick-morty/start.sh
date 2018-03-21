ced786f97aee877857e3e999559a9bdf5b72ff58
zVictor <victor0110@msn.com>
2018-03-21 16:46:20 +0100

    add extra clients to radiaction's example

--- packages/radiaction/examples/orders-rick-morty/start.sh
+++ packages/radiaction/examples/orders-rick-morty/start.sh
@@ -5,10 +5,10 @@ HOST_IP=`../host-ip.sh`
 
 npm i
 export HOST_IP && docker-compose up -d
-printf "giving extra time for containers to be fully up and running... "
-sleep 5
-printf "\033[0;32mdone\033[0m\n\n\n"
+printf "giving extra time for containers to be fully up and running... \n\n\n"
 
-babel-node distribution &
-xterm -hold -e 'babel-node application' &
+(sleep 2 && babel-node distribution) &
+(sleep 4 && xterm -hold -e 'babel-node application') &
+(sleep 4 && xterm -hold -e 'babel-node application') &
+(sleep 4 && xterm -hold -e 'babel-node application') &
 wait
